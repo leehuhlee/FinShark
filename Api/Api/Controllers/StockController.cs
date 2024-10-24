4f81fc0024f9924ce3887a860e6224c96965fdb3
leehuhlee <leehuhlee@gmail.com>
2024-10-24 20:59:30 +0200

    api

---
+++ Api/Api/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;    
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var stock = _context.Stocks.ToList();
+            return Ok(stock);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] string id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
