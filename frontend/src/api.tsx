b5d0fc53fbc41ae1f21ebb74d11446a4644c06a0
leehuhlee <leehuhlee@gmail.com>
2024-10-27 18:30:57 +0100

    BalanceSheet

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
+import { CompanyBalanceSheet, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
   data: CompanySearch[];
@@ -48,6 +48,17 @@ export const getIncomeStatement = async (query: string) => {
   try {
     const data = await axios.get<CompanyIncomeStatement[]>(
       `https://financialmodelingprep.com/api/v3/income-statement/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message from API: ", error.message);
+  }
+}
+
+export const getBalanceSheet = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyBalanceSheet[]>(
+      `https://financialmodelingprep.com/api/v3/balance-sheet-statement/${query}?apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
---
+++ frontend/src/components/BalanceSheet/BalanceSheet.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import { CompanyBalanceSheet, CompanyCashFlow } from '../../company';
+import { useOutletContext } from 'react-router';
+import { getBalanceSheet } from '../../api';
+import RatioList from '../RatioList/RatioList';
+
+type Props = {}
+
+const config = [
+  {
+    label: "Cash",
+    render: (company: CompanyBalanceSheet) => company.cashAndCashEquivalents,
+  },
+  {
+    label: "Inventory",
+    render: (company: CompanyBalanceSheet) => company.inventory,
+  },
+  {
+    label: "Other Current Assets",
+    render: (company: CompanyBalanceSheet) => company.otherCurrentAssets,
+  },
+  {
+    label: "Minority Interest",
+    render: (company: CompanyBalanceSheet) => company.minorityInterest,
+  },
+  {
+    label: "Other Non-Current Assets",
+    render: (company: CompanyBalanceSheet) => company.otherNonCurrentAssets,
+  },
+  {
+    label: "Long Term Debt",
+    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+  },
+  {
+    label: "Total Debt",
+    render: (company: CompanyBalanceSheet) => company.otherCurrentLiabilities,
+  },
+];
+
+const BalanceSheet = (props: Props) => {
+  const ticker = useOutletContext<string>();
+  const [balanceSheet, setBalanceSheet] = useState<CompanyBalanceSheet>();
+
+  useEffect(() => {
+    const getCompanyBalanceSheet = async() => {
+      const result = await getBalanceSheet(ticker!)
+      setBalanceSheet(result?.data[0]);
+    }
+    getCompanyBalanceSheet();
+  }, [])
+
+  return (
+    <>
+      { balanceSheet ? (
+        <RatioList config={config} data={balanceSheet}/>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
+  )
+}
+
+export default BalanceSheet
