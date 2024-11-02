d8d83adab5a2bca6429f012accd4ebdc4a61b9a6
leehuhlee <leehuhlee@gmail.com>
2024-11-02 22:54:08 +0100

    Register

---
+++ Api/Api/Migrations/20241102210559_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "655d9ae1-62b3-4900-b6b6-bfa8191681e8", null, "User", "USER" },
+                    { "8e79c3e0-58d6-4b98-88a4-dc78e3b5c7a1", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "655d9ae1-62b3-4900-b6b6-bfa8191681e8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8e79c3e0-58d6-4b98-88a4-dc78e3b5c7a1");
+        }
+    }
+}
