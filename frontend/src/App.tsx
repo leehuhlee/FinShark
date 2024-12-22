09b688fda40522b1d93df09729692ed2e0d3bf73
leehuhlee <leehuhlee@gmail.com>
2024-12-22 21:07:17 +0100

    AuthService

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   
-
   return (
     <>
       <Navbar/>
       <Outlet/>
+      <ToastContainer />
     </>
   );
 }
